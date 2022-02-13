5508f38e0d3a580be0aabc6445d80609b7a917ee
Puthipong <puthipong@bluebik.com>
2022-02-13 22:39:28 +0700

    Add ping times for more consistent

--- src/network-monitor.ts
+++ src/network-monitor.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 import * as ping from 'ping';
 import devices from './devices';
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
 	headers: {
@@ -10,34 +10,45 @@ const axiosInstance: AxiosInstance = axios.create({
 		'Accept-Encoding': 'gzip, deflate, br'
 	}
 });
+const PING_TIMES: number = parseInt(process.env.PING_TIMES);
+console.log(PING_TIMES);
+async function main() {
+	const promises: Array<Promise<string>> = Object.keys(devices).map(async (name) => {
+		let count = 0;
 
-function main() {
-	const promises: Array<Promise<string>> = [];
-	Object.keys(devices).forEach(function (name) {
-		const promise: Promise<string> = ping.promise.probe(devices[name]).then((response: ping.PingResponse) => {
-			if(!response.alive){
+		try {
+			for (let i = 0; i < PING_TIMES; i++) {
+				let pingResponse: ping.PingResponse = await ping.promise.probe(devices[name]);
+				console.log(name, i, pingResponse.alive);
+				if(pingResponse.alive){
+					count++;
+				}
+			}
+			console.log(name, count);
+
+			if(count < PING_TIMES / 2) {
 				return `${name} is down`;
 			}
-		}).catch((reason) => {
+		} catch(reason) {
 			return JSON.stringify(reason);
-		});
-
-		promises.push(promise);
+		}
 	});
 
-	Promise.all(promises).then((data: Array<string>) => {
-		console.log(new Date().toLocaleString());
-		data = data.filter(item => item !== undefined);
-		console.log(data);
-		axiosInstance.post(process.env.ALIVE_URL, {
-			userId: process.env.LINE_USER_ID,
-			data: data
-		})
-		.then(response => console.log(response.data))
-		.catch(error => {
+	console.log(new Date().toLocaleString());
+	let data: Array<string> = await Promise.all(promises);
+
+	data = data.filter(item => item !== undefined);
+	console.log(data);
+
+	try {
+		const response: AxiosResponse = await axiosInstance.post(process.env.ALIVE_URL, {
+				userId: process.env.LINE_USER_ID,
+				data: data
+			})
+		console.log(response.data);
+	}catch(error) {
 			console.log(error);
-		});
-	})
+	}
 
 	setTimeout(main, parseInt(process.env.WAIT_TIMEOUT_MIN) * 60000);
 }
